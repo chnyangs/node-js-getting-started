658cc6b420f233d267f1ef0fc6a6be99de215877
Xiangwen <xwy411@gmail.com>
2022-03-14 16:40:40 +1100

    update forwarding apu

--- src/routes/api.js
+++ src/routes/api.js
@@ -5,26 +5,18 @@ const fs = require('fs');
 
 router.post('/forwarding', async function (req, res, next) {
     try{
-        //get POST payload
-        let jsonData = "";
-        // console.log(req)
-        const data = req.body;
-        if(data.result === 'success'){
-            // append data to file
-            const logPath = path.join(__dirname, '../../log/log.txt');
-            let channel = req.body.channel
-            channel.feeds.forEach(feed =>{
-                feed['field'] = Object.values(feed)[1]
-            })
-            fs.appendFileSync(logPath, JSON.stringify(channel) + '\n', function(err) {
-                if(err) {
-                    res.end('ERROR');//mark request as error
-                }
-            });
-            res.end('SUCCESS');//mark request as successful
-        }else{
-            res.end('ERROR');//mark request as successful
-        }
+        // append data to file
+        const logPath = path.join(__dirname, '../../log/log.txt');
+        let channel = req.body.channel
+        channel.feeds.forEach(feed =>{
+            feed['field'] = Object.values(feed)[1]
+        })
+        fs.appendFileSync(logPath, JSON.stringify(channel) + '\n', function(err) {
+            if(err) {
+                res.end('ERROR');//mark request as error
+            }
+        });
+        res.end('SUCCESS');//mark request as successful
     }catch{
         res.end('ERROR');//mark request as error
     }
